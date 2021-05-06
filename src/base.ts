5e9e62e1d3aeeefcb617f007e15c352b406dcf05
magicD <musashi.miyamoto81@gmail.com>
2021-05-06 18:52:57 +0200

    Base, requestHandler error interceptor

--- src/base.ts
+++ src/base.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
+import { AxiosError, AxiosRequestConfig } from 'axios';
 import { bottle } from './services/bottle';
 
 // config type that match both request handlers
@@ -28,8 +28,38 @@ export abstract class Base {
         // init request handler
         this.requestHandlerService.init(config);
     }
+
+    private errorHandler(error:AxiosError) {
+        switch(true) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            case !!error.response:
+                console.log('server response error');
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            break;
+
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            case !!error.request:
+                console.log('server unresponsive error');
+                console.log(error.request);
+            break;
+
+            // Something happened in setting up the request that triggered an Error
+            default:
+                console.log('unknown ajax error', error.message);
+        }
+    }
     
     protected requestHandler(requestConfig:AxiosRequestConfig):Promise<any> {
-        return this.requestHandlerService.requestHandler(requestConfig);
+        const returnedPromise = this.requestHandlerService.requestHandler(requestConfig);
+
+        // intercept errors
+        returnedPromise.catch(this.errorHandler);
+        
+        return returnedPromise;
     }
 }
