33c4fe0c6c7fb2eaa0f08d5c880e5f4785f71d88
magicD <musashi.miyamoto81@gmail.com>
2021-05-11 09:08:58 +0200

    added getAllOrders endpoint (multi page query)

--- src/order/index.ts
+++ src/order/index.ts
@@ -4,7 +4,7 @@ import { SearchOrdersParams, OrdersExpandingKeys } from './types';
 
 const resourceName = 'orders';
 export class Order extends Base {
-    getOrders(params:SearchOrdersParams) {
+    async getOrders(params:SearchOrdersParams) {
         // response code 200 = all results, code 206 = single page in multi pages results
         return this.requestHandler({
             method: 'get',
@@ -14,21 +14,29 @@ export class Order extends Base {
 
     async getAllOrders(params:SearchOrdersParams) {
         //1. get first page with maxItemsPerPage
-        const firstQueryReq = await this.getOrders({ ...params, perPage: 10/*this.config.maxItemsPerPage*/, page: 1 });
+        const firstQueryReq = await this.getOrders({ ...params, perPage: this.config.maxItemsPerPage, page: 1 });
 
         //2. evaluate if more pages are available for this resource (response headers indicate how many results possible)
         const metaResourcesInfo = parseResponseHeadersMeta(firstQueryReq.headers);
-        const totalResourcePages = Math.ceil(metaResourcesInfo.totalResourceCount / 10/*metaResourcesInfo.maxPerPageCount*/);
+        const totalResourcePages = Math.ceil(metaResourcesInfo.totalResourceCount / this.config.maxItemsPerPage);
+        if(metaResourcesInfo.maxPerPageCount !== this.config.maxItemsPerPage) this.logger.warn(`${ metaResourcesInfo.resource } resource has a maxPerPageCount of ${ metaResourcesInfo.maxPerPageCount } (config is ${ this.config.maxItemsPerPage })`);
 
         //3. if no more pages return results, if more pages call all missing pages in parralel
         if(totalResourcePages === 1) return firstQueryReq;
         const missingPageIndexes = [...Array(totalResourcePages + 1).keys()].slice(2);
-        this.logger.info(`Need to extract ${ totalResourcePages -1 } more pages`);
-        this.logger.log(missingPageIndexes);
+        this.logger.info(`Need to extract ${ totalResourcePages - 1 } more pages`);
+        
         //4. once all results arrived merged response array and reply
+        const otherQueryReqs = await Promise.all(missingPageIndexes.map(pageIndex => this.getOrders({ ...params, perPage: this.config.maxItemsPerPage, page: pageIndex })));
+        const concatenatedResponses = otherQueryReqs.map(resp => resp.data);
+        
+        const allOrders = [...firstQueryReq.data, ...[].concat(...concatenatedResponses)];
+        firstQueryReq.data = allOrders;
+
+        return firstQueryReq;
     }
 
-    getOrderById(params:{ id:number, expanding?: Array<OrdersExpandingKeys> }) {
+    async getOrderById(params:{ id:number, expanding?: Array<OrdersExpandingKeys> }) {
         const clonedParams = { ...params };
         delete (clonedParams as any).id;
         
